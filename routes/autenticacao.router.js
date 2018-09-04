953dc94068ea8c512638d2d331542b016130496d
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-09-04 20:42:32 -0300

    documentação swagger de autenticação

--- routes/autenticacao.router.js
+++ routes/autenticacao.router.js
@@ -3,8 +3,48 @@ var router = express.Router();
 var academia = require('../controllers/authenticate.controller');
 
 
+/**
+ * @swagger
+ * /auth:
+ *   post:
+ *     tags:
+ *       - Autenticacao
+ *     description: Autenticar uma academia
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *        - in: body
+ *          name: user
+ *          schema:
+ *           type: object
+ *           required:
+ *             - nome
+ *           properties:
+ *             username:
+ *               type: string
+ *             password:
+ *               type: string
+ *     responses:
+ *      201:
+ *       description: Criado
+ *       schema:
+ *            $ref: '#/definitions/autenticacao'
+ */
 router.post('/', academia.autentica);
 
 
 
 module.exports = router;
+
+/**
+ * @swagger
+ * definition:
+ *   autenticacao:
+ *     properties:
+ *          username:
+ *              type: string
+ *          password:
+ *              type: string
+ */

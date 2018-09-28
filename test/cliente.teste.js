30c6e00342ebf1b8ad2c47627d70402d85d09753
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-09-27 21:55:38 -0300

    testes de academia

--- test/cliente.teste.js
+++ test/cliente.teste.js
@@ -1,17 +1,101 @@
-const express = require('express');
 const request = require('supertest');
 const app = require('../app');
 
 
-describe('Todos os clientes do sistema', function() {
-    describe('GET /cliente', function() { 
-        it('deve receber todos os clientes', function(done) { 
-          request(app)
-            .get('/cliente')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-          
-        });
-      });
+//==================== cliente API test ====================
+// É preciso estar logado com a academia para poder rodar os testes
+
+/**
+ * Teste para obter todas as academias
+ */
+describe('GET /cliente', function () {
+  it('responder com json contendo uma lista de todas os clientes da academia logada', function (done) {
+      request(app)
+          .get('/cliente')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200, done);
+  });
+});
+
+/**
+* Teste para obter sucesso ao encontrar academia
+*/
+describe('GET /academia/:id', function () {
+  it('responder com json contendo uma lista de todas as academias', function (done) {
+      request(app)
+          .get('/academia/5b987e0050758f4cc54c2619')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200, done);
+  });
+});
+
+/**
+* Teste para obter um erro de não encontrar id de uma academia
+*/
+describe('GET /academia/:id', function () {
+  it('responder com o usuário json não encontrado', function (done) {
+      request(app)
+          .get('/academia/12')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(404) //expecting HTTP status code
+          .expect('{"message":"Não existe academia com id 12"}') // expecting content value
+          .end((err) => {
+              if (err) return done(err);
+              done();
+          });
+  });
+});
+
+/**
+* Teste de um cadastro de academiai
+*/
+describe('POST /academia', function () {
+  let academia = {
+      "id": "1",
+      "nome": "Academia Teste",
+      "endereco": "rua local",
+      "cnpj": "656566532",
+      "username": "academiaTeste",
+      "password": "123456"
+  }
+  it('respond with 200 created', function (done) {
+      request(app)
+          .post('/academia')
+          .send(academia)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err) => {
+              if (err) return done(err);
+              done();
+          });
+  });
+});
+
+/**
+* Teste de uma academia com erro
+*/
+describe('POST /users', function () {
+  let data = {
+    "endereco": "rua local",
+    "cnpj": "656566532",
+    "username": "academiaTeste",
+    "password": "123456"
+  }
+  it('respond with 400 not created', function (done) {
+      request(app)
+          .post('/academia')
+          .send(data)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .expect('{"message":"Nome não pode ser vazio!"}')
+          .end((err) => {
+              if (err) return done(err);
+              done();
+          });
+  });
 });
